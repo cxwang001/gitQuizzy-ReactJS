b9234d1fc10bea98da01d31a1fd53efe25848e51
AlexDJ <deejxue@gmail.com>
2017-08-21 12:06:19 -0700

    initial server and webpack.config files

---
+++ server.js
@@ -0,0 +1,93 @@
+// Include Server Dependencies
+var express = require("express");
+var bodyParser = require("body-parser");
+var logger = require("morgan");
+var mongoose = require("mongoose");
+
+var Article = require('./models/User.js');
+
+// Create Instance of Express
+var app = express();
+// Sets an initial port. We'll use this later in our listener
+var PORT = process.env.PORT || 3000;
+
+// Run Morgan for Logging
+app.use(logger("dev"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+// This is how we serve/route our static pages 
+app.use(express.static("public"));
+
+// -------------------------------------------------
+
+// MongoDB Configuration configuration (Change this URL to your own DB)
+mongoose.connect("mongodb://localhost/gitquizzly");
+var db = mongoose.connection;
+console.log(db);
+
+db.on("error", function(err) {
+  console.log("Mongoose Error: ", err);
+});
+
+db.once("open", function() {
+  console.log("Mongoose connection successful.");
+});
+
+// -------------------------------------------------
+
+// Main "/" Route. This will redirect the user to our rendered React application
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/public/index.html");
+});
+
+app.get('/api/user', function(req, res) {
+
+  User.find({})
+    .exec(function(err, doc){
+
+      if(err){
+        console.log(err);
+      }
+      else {
+        res.send(doc);
+      }
+    })
+});
+
+app.post('/api/user', function(req, res){
+  var newUser = new User(req.body);
+
+  var name = req.body.name;
+  var win = req.body.win;
+  var loss = req.body.loss;
+
+  newUser.save(function(err, doc){
+    if(err){
+      console.log(err);
+    } else {
+      res.send(doc._id);
+    }
+  });
+});
+
+app.delete('/api/user/', function(req, res){
+
+  var url = req.param('url');
+
+  User.find({"url": url}).remove().exec(function(err, data){
+    if(err){
+      console.log(err);
+    }
+    else {
+      res.send("Deleted");
+    }
+  });
+});
+
+// -------------------------------------------------
+// Turn on PORT Listener
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
+});
